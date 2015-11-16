192828bd3937648b39645cbe73736c74f206acde
liuy0813 <liuy0813@gmail.com>
2015-11-16 09:51:19 +0800

    delete password

--- SendMailForSwan.m
+++ SendMailForSwan.m
@@ -8,6 +8,6 @@ function SendMail(subject,content)
 %
 TargetAddress='253588604@qq.com';
 SourceAddress='liu08_13@126.com';
-password='813428liu';
+password='';
 Attachments=[];
 MatlabSendMailGeneral(subject,content,TargetAddress, Attachments,SourceAddress,password)
